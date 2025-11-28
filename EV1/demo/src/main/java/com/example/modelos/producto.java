9717a0508fa00846666db1de243f20c2cf83ce99
Gerboii <ger.doblas@gmail.com>
2025-11-28 09:49:42 +0100

    Update

---
+++ EV1/demo/src/main/java/com/example/modelos/producto.java
@@ -0,0 +1,22 @@
+package com.example.modelos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@JsonIgnoreProperties(ignoreUnknown=true)
+
+public class producto {
+// o Productos: id (pk), nombre, descripción, cantidad, precio.
+//o Productos_Fav: id (pk), id_producto (fk)
+int id;
+String name;
+String description;
+double price;
+int stock;
+}
---
+++ EV1/demo/src/main/java/com/example/modelos/productoResponse.java
@@ -0,0 +1,6 @@
+package com.example.modelos;
+
+public class productoResponse {
+
+    List <producto> prodList;
+}
