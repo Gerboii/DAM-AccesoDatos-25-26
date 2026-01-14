ca306b0d1f5651276f25335dff88da20ce0d2988
Gerboii <ger.doblas@gmail.com>
2026-01-14 20:33:51 +0100

    Update info

--- EV2/demo/src/main/java/com/example/Main.java
+++ EV2/demo/src/main/java/com/example/Main.java
@@ -1,5 +1,37 @@
 package com.example;
+/*Gestión de Ligas:
+- Crear, editar y eliminar ligas.
+- Asignar equipos a las ligas.
+- Consultar la información de las ligas disponibles.*/
 
+/*Gestión de Equipos:
+- Registrar nuevos equipos.
+- Modificar información de equipos existentes.
+- Eliminar equipos de una liga.*/
+
+//Gestión de Jugadores:- Registrar nuevos jugadores, indicando los datos necesarios.
+
+//Gestión de Entrenadores: Registra entrenadores a los equipos existentes
+
+/*Las relaciones que se necesitan en el programa están marcadas por los siguientes
+supuestos:
+- Un equipo tan solo puede estar dado de alta en una liga al mismo tiempo
+- Una liga está formada por multitud de equipos
+- Un equipo está formado por muchos jugadores y un jugador solo puede jugar en
+un equipo
+- Un jugador solo puede trabajar en una ubicación
+- Un equipo tiene un entrenador */
+
+/*Una vez creada la estructura realiza las siguientes acciones:
+- Crea 1 liga
+- Crea 3 equipos y asócialos a la liga
+- Crea 6 jugadores y asócialos a los equipos
+- Ficha un par de jugadores para otro equipo
+- Crea tres entrenadores y asócialos a un equipo
+- Muestra datos de todos los equipos
+- Muestra los jugadores de un equipo
+- Muestra los equipos de una liga
+- Muestra todos los entrenadores de los equipos de una liga */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
