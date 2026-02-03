20f400467f794c381b32c176cf752b34b36d41ae
Gerboii <ger.doblas@gmail.com>
2026-02-03 21:53:41 +0100

    Update

---
+++ EV2TEST/demo/src/main/java/com/example/dao/AlumnoDaoImp.java
@@ -0,0 +1,9 @@
+package com.example.dao;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class AlumnoDaoImp {
+    private Session session;
+    private Transaction transaction;
+}
---
+++ EV2TEST/demo/src/main/java/com/example/dao/ProfesorDaoImp.java
@@ -0,0 +1,9 @@
+package com.example.dao;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class ProfesorDaoImp {
+    private Session session;
+    private Transaction transaction;
+}
