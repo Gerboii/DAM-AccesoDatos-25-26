160037628e9cbf276a897a0de09ba5dbe901a0af
Gerboii <ger.doblas@gmail.com>
2026-02-08 22:25:37 +0100

    Update HibernateUtil

--- EV2TEST/demo/src/main/java/com/example/util/HibernateUtil.java
+++ EV2TEST/demo/src/main/java/com/example/util/HibernateUtil.java
@@ -5,9 +5,21 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private static SessionFactory sessionFactory ;
     //No puedo/quiero crear las session en el main porque se va a encargar de lanzar la vista
-    // 1-> Creas una session factory
-    SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-    // 2-> Crea la session
-    Session session = sessionFactory.openSession();
+    //TODO Completar metodo getSession devuelve una conexion
+    public static SessionFactory getSession() {
+        if(sessionFactory == null){
+            try {
+                // 1-> Creas una session factory
+                SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+                // 2-> Crea la session
+                Session session = sessionFactory.openSession();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return sessionFactory;
+    }
+
 }
