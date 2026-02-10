217b453c6a6de1bf17221a20007b384cc660263b
Gerboii <ger.doblas@gmail.com>
2026-02-10 20:35:55 +0100

    Implementa NotaDao

--- EV2TEST/demo/src/main/java/com/example/dao/NotaDaoImp.java
+++ EV2TEST/demo/src/main/java/com/example/dao/NotaDaoImp.java
@@ -12,53 +12,71 @@ import java.util.List;
 public class NotaDaoImp {
     private Session session;
     private Transaction transaction;
-        /*TODO  Método guardarNota() recibe las notas de la tabla y las guarda en BD
-          * OPCIONAL- Método mostrarEvaluacion() recibe dniAlumno, devuelve todas las notas asociadas.*/
 
     //Recibe una asignatura y devuelve el listado de alumnos con su nota
     public List<Nota> alumAsignatura(Asignatura asignatura){
         session = HibernateUtil.getSessionFactory().openSession();
         transaction = session.beginTransaction();
-        // Sentencia HQL
-        Query<Nota> query = session.createQuery("FROM Nota n WHERE n.asignatura = :asig", Nota.class);
-        query.setParameter("asig", asignatura);
-        List<Nota> listaResultante = query.getResultList();
-        // Impactar y cerrar
-        transaction.commit();
-        session.close();
-
-        return listaResultante;
+        try {
+            // Sentencia HQL
+            Query<Nota> query = session.createQuery("FROM Nota n WHERE n.asignatura = :asig", Nota.class);
+            query.setParameter("asig", asignatura);
+            List<Nota> listaResultante = query.getResultList();
+            // Impactar y cerrar
+            transaction.commit();
+            return listaResultante;
+        }
+        catch(Exception e){
+            throw new RuntimeException(e);
+        }
+        finally {
+            session.close();
+        }
     }
 
     //Recibe dniAlumno y asignatura, devuelve la nota.
-    public Nota mostrarNota(Asignatura asignatura, Alumno alumno){
+    public Nota mostrarNota(Asignatura asignatura, Alumno alumno) {
         session = HibernateUtil.getSessionFactory().openSession();
         transaction = session.beginTransaction();
-        // Sentencia HQL
-        Query<Nota> query = session.createQuery("FROM Nota n WHERE n.asignatura = :asig AND n.alumno = :alum", Nota.class);
-        query.setParameter("asig", asignatura);
-        query.setParameter("alum", alumno);
-        Nota notaResultante = query.getSingleResult();
-        // Impactar y cerrar
-        transaction.commit();
-        session.close();
-
-        return notaResultante;
+        try{
+            // Sentencia HQL
+            Query<Nota> query = session.createQuery("FROM Nota n WHERE n.asignatura = :asig AND n.alumno = :alum", Nota.class);
+            query.setParameter("asig", asignatura);
+            query.setParameter("alum", alumno);
+            Nota notaResultante = query.getSingleResult();
+            // Impactar y cerrar
+            transaction.commit();
+            return notaResultante;
+        }
+        catch(Exception e){
+            throw new RuntimeException(e);
+        }
+        finally {
+            session.close();
+        }
     }
-    //TODO Arreglar
-    /*public List<Nota> mostrarEvaluacion(Asignatura asignatura){
+
+    //Recibe dniAlumno devuelve todas las notas
+    public List<Nota> mostrarEvaluacion(String dniAlumno){
         session = HibernateUtil.getSessionFactory().openSession();
         transaction = session.beginTransaction();
-        // Sentencia HQL
-        Query<Nota> query = session.createQuery("FROM Nota n WHERE n.alumno = :alum", Nota.class);
-        query.setParameter("alum", );
-        List<Nota> listaResultante = query.getResultList();
-        // Impactar y cerrar
-        transaction.commit();
-        session.close();
+        try{
+            // Sentencia HQL
+            Query<Nota> query = session.createQuery("FROM Nota n WHERE n.alumno.dniAlumno = :alum", Nota.class);
+            query.setParameter("alum", dniAlumno);
+            List<Nota> listaResultante = query.getResultList();
+            // Impactar y cerrar
+            transaction.commit();
+            return listaResultante;
+        }
+        catch(Exception e){
+            throw new RuntimeException(e);
+        }
+        finally {
+            session.close();
+        }
+    }
 
-        return listaResultante;
-    }*/
     //Toma una lista de notas y actualiza la BD
     public void guardarNota(List<Nota> listaNotas){
         session = HibernateUtil.getSessionFactory().openSession();
