d2b6f8a7fad0a9c679ced17f168cedce2dc007fe
Gerboii <ger.doblas@gmail.com>
2026-02-10 21:45:00 +0100

    Update

--- EV2TEST/demo/src/main/java/com/example/controller/ProfesorController.java
+++ EV2TEST/demo/src/main/java/com/example/controller/ProfesorController.java
@@ -8,6 +8,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 public class ProfesorController {
@@ -41,7 +43,25 @@ public class ProfesorController {
     void salir(ActionEvent event) {
         System.exit(0);
     }
+    //TODO Arreglar
+    /*@FXML
+    public void initialize() {
+        // Configurar columnas
+        colNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        //PropertyValueFactory. Indica qué propiedad del objeto tiene que usar, en este cado llama al metodo get de la clase Alumno
+        //setCellValueFactory. Define cómo obtener el valor de la columna.
+        colPass.setCellValueFactory(new PropertyValueFactory<>("contrasenia"));
 
+        tablaAlumnos.setItems(lista);
+        tablaAlumnos.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+        // Cargar ComboBox al iniciar
+        cargarComboBox();
+    }
+    //TODO Crear
+    private void cargarComboBox() {
+    }
+*/
     public void setProfesor(Profesor profe) {
         this.profesorLog=profe;
     }
